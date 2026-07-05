aa2a0cf329006d80595ba8f0cf5e11fb128ead5d
Nagendra <nagendrasahni653@gmail.com>
2026-07-05 15:04:54 +0530

    Phase 1.6 PostgreSQL integration completed

--- ai-shopping-agent/backend/app/main.py
+++ ai-shopping-agent/backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from app.database.base import Base
+from app.database.connection import engine
+
+from app.models.user import User
 
 from app.core.config import settings
 from app.core.config import settings
 
 print("Database URL:", settings.DATABASE_URL)
 
-
+Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,

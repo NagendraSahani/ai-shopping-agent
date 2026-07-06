188bf0e691ea245cc74c8f7291c8d67498baacda
Nagendra <nagendrasahni653@gmail.com>
2026-07-06 13:17:09 +0530

    Remove duplicate root files and keep project inside ai-shopping-agent

--- test_serpapi.py
+++
@@ -1,9 +0,0 @@
-from app.tools.serpapi_tool import SerpAPITool
-
-tool = SerpAPITool()
-
-result = tool.search(
-    "Best laptop under 60000"
-)
-
-print(result)
--- test_tavily.py
+++
@@ -1,9 +0,0 @@
-from app.tools.tavily_tool import TavilyTool
-
-tool = TavilyTool()
-
-result = tool.search(
-    "Best laptop under 60000 India"
-)
-
-print(result)
